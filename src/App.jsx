a08964a1921c060e907d23c11ceab7757b42d186
francisfin-lang <francis.fin@gmail.com>
2026-08-04 22:22:48 +0530

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -39,6 +39,14 @@ function InvitationNotFoundState() {
 
 function AppRoutes() {
   const location = useLocation();
+  useEffect(() => {
+  const params = new URLSearchParams(window.location.search);
+  const redirect = params.get("redirect");
+
+  if (redirect) {
+    window.history.replaceState({}, "", redirect);
+  }
+}, []);
   const inviteId = getInviteIdFromUrl();
   const shouldLoadInvitation = !["/login", "/admin"].includes(location.pathname);
 
